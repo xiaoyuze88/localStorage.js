5591f6ee4f6f0aa823d85fddc65896e5f9423b9b
xyz <xiaoyuze88@gmail.com>
2014-12-15 23:01:24 +0800

    upload

--- README.md
+++ README.md
@@ -2,3 +2,39 @@ localStorage.js
 ===============
 
 Tiny localStorage module, check and try to use native localStorage api if the browser is supported, otherwise use cookie instead.
+
+## QUICK START
+
+In AMD, use it like:
+
+    require('localStorageJS',function(localStorageJS){
+        localStorageJS.set('yourkey','yourvalue');
+
+        localStorageJS.get('yourkey');    //yourvalue
+    });
+
+Otherwise:
+    
+`localStorageJS` just explodes to the global `window` object.  
+Use it directly.
+
+    localStorageJS.set('yourkey','yourvalue');
+
+    localStorageJS.get('yourkey');    //yourvalue
+
+
+## METHODS
+
+### localStorageJS.set(key,value)
+
+### localStorageJS.get(key)
+
+### localStorageJS.remove(key)
+
+### localStorageJS.config(options)
+
+    options(Object) : {
+        prefix : (String), you can set your own prefix when storaging in localStorage. Default to be `__localStorageJS`
+        expires : (Number) expires hours, default to be 24
+    }
+
---
+++ src/localStorage.js
@@ -0,0 +1,141 @@
+/**
+ *      localStorage.js
+ *
+ *      Tiny localStorage module for mobile, check and try to use native localStorage api first,
+ *      if localStorage is not available(e.g : IOS safari security mode or localStorage is disabled), use cookie instead.
+ *
+ *      Author: xiaoyuze88@gmail.com (https://github.com/xiaoyuze88/localStorage.js)
+ *
+ *
+ */
+
+;(function(factory) {
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+    module.exports = factory();
+  else if (typeof define == "function" && define.amd) // AMD
+    return define([], factory);
+  else // Plain browser env
+    this.localStorageJS = factory();
+})(function(){
+
+    'use strict';
+
+    var localStorageJS = (function() {
+
+        var isLocalstorageEnable = (function() {
+                try {
+                    localStorage.setItem("__test", '123');
+                    localStorage.removeItem("__test");
+                    return true;
+                } catch (e) {
+                    return false;
+                }
+            })(),
+            options = {
+                prefix: '__localStorageJS',
+                // hours
+                expires: 24
+            };
+            
+
+
+        function config(opts) {
+
+            if (!opts) return;
+
+            for (var i in options) {
+                if (opts.hasOwnProperty(i)) {
+                    options[i] = opts[i];
+                }
+            }
+        }
+
+        var _cookie = {
+            get: function(key) {
+                var arr = document.cookie.match(new RegExp("(^| )" + key + "=([^;]*)(;|$)"));
+                if (arr)
+                    return unescape(arr[2]);
+                else
+                    return null;
+            },
+            set: function(key, value) {
+                var exp = new Date();
+                exp.setTime(exp.getTime() + options.expires * 60 * 60 * 1000);
+                document.cookie = key + "=" + escape(value) + ";expires=" + exp.toGMTString();
+            },
+            remove: function(key) {
+                var exp = new Date();
+                exp.setTime(exp.getTime() - 1);
+                documentcookie = key + "=;expires=" + exp.toUTCString();
+            }
+        }
+
+        var _localstorage = {
+            get: function(key) {
+                var name = options.prefix + key,
+                    data = window.localStorage.getItem(name);
+
+                if (data) {
+                    try {
+                        data = JSON.parse(data);
+
+                        if(data.expires - Date.now() > 0) {
+                            return data.data;
+                        }
+                        // expired
+                        else {
+                            window.localStorage.removeItem(name);
+                            return null;
+                        }
+                    }
+                    catch(e) {
+                        return null;
+                    }
+                }
+                else {
+                    return null;
+                }
+            },
+            set: function(key, value) {
+                var name = options.prefix + key,
+                    storage = {
+                        data: value,
+                        expires: Date.now() + options.expires * 60 * 60 * 1000
+                    };
+
+                window.localStorage.setItem(name, JSON.stringify(storage));
+            },
+            remove: function(key) {
+                var name = options.prefix + key;
+                window.localStorage.removeItem(name);
+            }
+        }
+
+        var func = isLocalstorageEnable ? _localstorage: _cookie;
+
+        function set(key, value) {
+            if (!key || !value) return false;
+            return func['set'](key, value);
+        }
+
+        function get(key) {
+            if (!key) return null;
+            return func['get'](key);
+        }
+
+        function remove(key) {
+            if (!key) return false;
+            return func['remove'](key);
+        }
+
+        return {
+            config: config,
+            set: set,
+            get: get,
+            remove: remove
+        }
+
+    })();
+
+    return localStorageJS;
+});
